89580c0f693ab286d7a6c48dc3b69249efb06ac0
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-06-07 15:55:54 +0800

    Create Key.java

---
+++ src/main/java/com/github/dev/objectnotation/Key.java
@@ -0,0 +1,19 @@
+package com.github.dev.objectnotation;
+
+/**
+ * Parse key.
+ */
+final class Key implements IntToFunction {
+
+	private final Parser parser;
+
+	Key(Parser parser) {
+		this.parser = parser;
+	}
+
+	@Override
+	public IntToFunction apply(int i) {
+		
+	}
+
+}
