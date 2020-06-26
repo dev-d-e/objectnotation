e57dea258fca14db3dec0559b7e6c808ecaa9fd8
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-06-26 14:46:17 +0800

    Update Parser.java

--- src/main/java/com/github/dev/objectnotation/Parser.java
+++ src/main/java/com/github/dev/objectnotation/Parser.java
@@ -4,20 +4,33 @@ import java.util.Objects;
 import java.util.function.IntConsumer;
 
 /**
- * 
+ * Parser.
  */
 final class Parser {
 
+	/**
+	 * counter
+	 */
 	int n = -1;
+
+	/**
+	 * row number
+	 */
+	private int row = 1;
 
 	final IntStringConsumer keyConsumer;
 	final IntConsumer valueConsumer;
 
-	private IntToFunction currentFunction;
+	final HeaderReader header = new HeaderReader(this);
+	final OffsetReader offset = new OffsetReader(this);
+	final Key key = new Key(this);
+	final Value value = new Value(this);
+	final NextLineReader next = new NextLineReader(this);
+	private IntToFunction currentFunction = header;
 
 	/**
 	 * Constructs a {@code Parser} with two consumers.
-	 * 
+	 *
 	 * @param keyConsumer   the consumer of the key.
 	 * @param valueConsumer the consumer of the value.
 	 */
@@ -31,6 +44,14 @@ final class Parser {
 	public void apply(int i) {
 		n++;
 		currentFunction = currentFunction.apply(i);
+	}
+
+	void addRow() {
+		row++;
+	}
+
+	int getRow() {
+		return row;
 	}
 
 }
