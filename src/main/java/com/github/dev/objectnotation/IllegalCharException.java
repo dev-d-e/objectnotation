e50412abc54e8790b9e90ddc32a631666d19ace6
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-06-26 14:42:55 +0800

    Update IllegalCharException.java

--- src/main/java/com/github/dev/objectnotation/IllegalCharException.java
+++ src/main/java/com/github/dev/objectnotation/IllegalCharException.java
@@ -8,19 +8,22 @@ public class IllegalCharException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * Constructs a IllegalCharException with no detail message.
-	 */
-	public IllegalCharException() {
-		super();
-	}
-
-	/**
 	 * Constructs a IllegalCharException with the specified detail message.
 	 *
 	 * @param s the detail message.
 	 */
 	public IllegalCharException(String s) {
 		super(s);
+	}
+
+	/**
+	 * Constructs a IllegalCharException.
+	 * 
+	 * @param row the row number.
+	 * @param col the column number.
+	 */
+	public IllegalCharException(int row, int col) {
+		super(new StringBuilder("illegal char is probably at(").append(row).append(',').append(col).append(')').toString());
 	}
 
 }
