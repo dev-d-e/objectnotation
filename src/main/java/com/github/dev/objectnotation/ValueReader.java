3a0b4d4dce2166141302f83ef6c56067307ef14d
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-06-26 14:50:14 +0800

    Create ValueReader.java

---
+++ src/main/java/com/github/dev/objectnotation/ValueReader.java
@@ -0,0 +1,30 @@
+package com.github.dev.objectnotation;
+
+/**
+ * Read Value.
+ */
+final class ValueReader implements IntToFunction {
+
+	private final ValueParser parser;
+
+	ValueReader(ValueParser parser) {
+		this.parser = parser;
+	}
+
+	@Override
+	public IntToFunction apply(int i) {
+		if (i == -1) {
+			parser.valueConsumer.accept(-1);
+			return this;
+		}
+		char c = (char) i;
+		if (c == '\\') {
+			return parser.backslashConverter;
+		} else if (c == '&') {
+			return parser.quoteValue;
+		}
+		parser.valueConsumer.accept(i);
+		return this;
+	}
+
+}
