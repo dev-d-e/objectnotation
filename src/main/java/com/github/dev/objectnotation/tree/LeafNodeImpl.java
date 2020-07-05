b22bf433021a693267558069d627df41a87831fb
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-07-05 16:53:23 +0800

    Create LeafNodeImpl.java

---
+++ src/main/java/com/github/dev/objectnotation/tree/LeafNodeImpl.java
@@ -0,0 +1,54 @@
+package com.github.dev.objectnotation.tree;
+
+import java.util.Iterator;
+
+import com.github.dev.objectnotation.value.Entity;
+
+/**
+ * Leaf node.
+ */
+class LeafNodeImpl extends AbstractNode {
+
+	LeafNodeImpl(int offset, String key, Entity entity) {
+		super(offset, key);
+		setEntity(entity);
+	}
+
+	@Override
+	public boolean isBranch() {
+		return false;
+	}
+
+	/**
+	 * Returns null.
+	 */
+	@Override
+	public Iterator<Node> iterator() {
+		return null;
+	}
+
+	/**
+	 * Returns null.
+	 */
+	@Override
+	public Node[] toArray() {
+		return null;
+	}
+
+	/**
+	 * Returns null.
+	 */
+	@Override
+	public Node node(int index) {
+		return null;
+	}
+
+	/**
+	 * Do not add.
+	 */
+	@Override
+	public Node add(Node node) {
+		return null;
+	}
+
+}
