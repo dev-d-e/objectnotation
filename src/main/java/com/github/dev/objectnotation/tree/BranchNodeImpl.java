af7aa061a70a35f080081dda90e90a93da403c79
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-07-05 16:52:50 +0800

    Create BranchNodeImpl.java

---
+++ src/main/java/com/github/dev/objectnotation/tree/BranchNodeImpl.java
@@ -0,0 +1,46 @@
+package com.github.dev.objectnotation.tree;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Branch node.
+ */
+class BranchNodeImpl extends AbstractNode {
+
+	private List<Node> nodes = new ArrayList<>();
+
+	BranchNodeImpl(int offset, String key) {
+		super(offset, key);
+	}
+
+	@Override
+	public boolean isBranch() {
+		return true;
+	}
+
+	@Override
+	public Iterator<Node> iterator() {
+		return nodes.iterator();
+	}
+
+	@Override
+	public Node[] toArray() {
+		return nodes.toArray(new Node[nodes.size()]);
+	}
+
+	@Override
+	public Node node(int index) {
+		Objects.checkIndex(index, nodes.size());
+		return nodes.get(index);
+	}
+
+	@Override
+	public Node add(Node node) {
+		nodes.add(node);
+		return this;
+	}
+
+}
