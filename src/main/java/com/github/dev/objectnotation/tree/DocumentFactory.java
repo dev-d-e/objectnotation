89e0942fbb4af9cec49bbc07ff7eb251b801e244
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-07-05 16:54:02 +0800

    Create DocumentFactory.java

---
+++ src/main/java/com/github/dev/objectnotation/tree/DocumentFactory.java
@@ -0,0 +1,70 @@
+package com.github.dev.objectnotation.tree;
+
+import java.util.Objects;
+
+import com.github.dev.objectnotation.value.Entity;
+
+/**
+ * Document factory.
+ */
+public class DocumentFactory {
+
+	private final Document document = new DocumentImpl();
+	private Node last;
+
+	public Document getDocument() {
+		return document;
+	}
+
+	public void addBranch(int offset, String key) {
+		Objects.requireNonNull(key);
+		if (offset < 0) {
+			return;
+		}
+		if (offset == 0) {
+			Node node = new BranchNodeImpl(offset, key);
+			document.add(node);
+			last = node;
+			return;
+		}
+		if (last instanceof LeafNodeImpl) {
+			return;
+		}
+		Node node = new BranchNodeImpl(offset, key);
+		if (offset == last.getOffset()) {
+			node.setParent(last.getParent());
+			last.getParent().add(node);
+		} else {
+			node.setParent(last);
+			last.add(node);
+		}
+		last = node;
+	}
+
+	public void addLeaf(int offset, String key, Entity entity) {
+		Objects.requireNonNull(key);
+		Objects.requireNonNull(entity);
+		if (offset < 0) {
+			return;
+		}
+		if (offset == 0) {
+			Node node = new LeafNodeImpl(offset, key, entity);
+			document.add(node);
+			last = node;
+			return;
+		}
+		if (last instanceof LeafNodeImpl) {
+			return;
+		}
+		Node node = new LeafNodeImpl(offset, key, entity);
+		if (offset == last.getOffset()) {
+			node.setParent(last.getParent());
+			last.getParent().add(node);
+		} else {
+			node.setParent(last);
+			last.add(node);
+		}
+		last = node;
+	}
+
+}
