74d1c9fb3fbedcfaf4e2c6ea94b24f397e4e8018
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-07-05 16:49:09 +0800

    Create Node.java

---
+++ src/main/java/com/github/dev/objectnotation/tree/Node.java
@@ -0,0 +1,69 @@
+package com.github.dev.objectnotation.tree;
+
+import com.github.dev.objectnotation.value.Entity;
+
+/**
+ * Node in a tree.
+ */
+public interface Node extends Cloneable, Iterable<Node> {
+
+	/**
+	 * Returns parent node or null if it is the root node.
+	 */
+	Node getParent();
+
+	/**
+	 * Set parent node.
+	 * 
+	 * @param node the parent.
+	 */
+	Node setParent(Node node);
+
+	/**
+	 * Returns true if this node is branch node.
+	 */
+	boolean isBranch();
+
+	/**
+	 * Returns all child nodes.
+	 */
+	Node[] toArray();
+
+	/**
+	 * Returns node at the specified index position.
+	 * 
+	 * @param index the index of the node.
+	 */
+	Node node(int index);
+
+	/**
+	 * Add a child node.
+	 * 
+	 * @param node a child node.
+	 */
+	Node add(Node node);
+
+	/**
+	 * Returns offset number.
+	 */
+	int getOffset();
+
+	/**
+	 * Returns key.
+	 */
+	String getKey();
+
+	/**
+	 * Returns entity.
+	 * 
+	 */
+	Entity getEntity();
+
+	/**
+	 * Set entity.
+	 * 
+	 * @param entity the entity.
+	 */
+	Node setEntity(Entity entity);
+
+}
