e90dddbd000b9d93f01b6a469d5c73d916062eb1
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-07-12 15:47:51 +0800

    Update ObjectReader.java

--- src/main/java/com/github/dev/objectnotation/ObjectReader.java
+++ src/main/java/com/github/dev/objectnotation/ObjectReader.java
@@ -13,7 +13,16 @@ public class ObjectReader implements IntToFunction {
 
 	@Override
 	public IntToFunction apply(int i) {
-		return parser.valueReader;
+		char c = (char) i;
+		if (c == '\r' || c == '\n') {
+			throw new IllegalCharException("illegal char.");
+		} else if (c == '}') {
+			parser.valueConsumer.accept(-1);
+			parser.valueConsumer.opt(0);
+			return parser.valueReader;
+		}
+		parser.valueConsumer.accept(i);
+		return this;
 	}
 
 }
