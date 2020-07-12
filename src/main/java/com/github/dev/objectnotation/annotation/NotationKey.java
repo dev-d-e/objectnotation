aa8310bd60392e03725b829f00b4a7e71196fd33
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-07-12 15:50:57 +0800

    Create NotationKey.java

---
+++ src/main/java/com/github/dev/objectnotation/annotation/NotationKey.java
@@ -0,0 +1,16 @@
+package com.github.dev.objectnotation.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface NotationKey {
+
+	String value() default "";
+
+	String global() default "";
+
+}
