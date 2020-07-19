b1c540ae2b35e3a49ccad2526ceba5526c0111df
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-07-19 20:46:49 +0800

    Update HtmlConverter.java

--- src/main/java/com/github/dev/objectnotation/html/HtmlConverter.java
+++ src/main/java/com/github/dev/objectnotation/html/HtmlConverter.java
@@ -1,8 +1,16 @@
 package com.github.dev.objectnotation.html;
 
+import com.github.dev.objectnotation.tree.Document;
+
 /**
  * Converter.
  */
 public class HtmlConverter {
+
+	public static String convert(CharSequence charSequence) {
+		Document document = DocumentInvoker.accept(charSequence);
+		TagHtml t = new TagHtml(document.node(0));
+		return t.asHtml();
+	}
 
 }
