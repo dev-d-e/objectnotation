c6bf5f83249fdc1631b4625fcb09512553934603
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-07-25 16:51:34 +0800

    Update TagBody.java

--- src/main/java/com/github/dev/objectnotation/html/TagBody.java
+++ src/main/java/com/github/dev/objectnotation/html/TagBody.java
@@ -1,8 +1,35 @@
 package com.github.dev.objectnotation.html;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.github.dev.objectnotation.tree.Node;
 
+/**
+ * body
+ */
 class TagBody extends TagAbstractImpl {
+
+	private static final Set<String> ATTRIBUTES = new HashSet<>();
+
+	static {
+		ATTRIBUTES.add("onafterprint");
+		ATTRIBUTES.add("onbeforeprint");
+		ATTRIBUTES.add("onbeforeunload");
+		ATTRIBUTES.add("onhashchange");
+		ATTRIBUTES.add("onlanguagechange");
+		ATTRIBUTES.add("onmessage");
+		ATTRIBUTES.add("onmessageerror");
+		ATTRIBUTES.add("onoffline");
+		ATTRIBUTES.add("ononline");
+		ATTRIBUTES.add("onpagehide");
+		ATTRIBUTES.add("onpageshow");
+		ATTRIBUTES.add("onpopstate");
+		ATTRIBUTES.add("onrejectionhandled");
+		ATTRIBUTES.add("onstorage");
+		ATTRIBUTES.add("onunhandledrejection");
+		ATTRIBUTES.add("onunload");
+	}
 
 	protected TagBody(Node node) {
 		super(node);
@@ -10,19 +37,7 @@ class TagBody extends TagAbstractImpl {
 
 	@Override
 	protected boolean isAttribute(String str) {
-		return false;
-	}
-
-	@Override
-	protected Tag createTag(String str, Node node) {
-		switch (str) {
-		case "div":
-			return new TagDiv(node);
-		case "form":
-			return new TagForm(node);
-		default:
-			return null;
-		}
+		return ATTRIBUTES.contains(str);
 	}
 
 }
