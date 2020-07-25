78ca23898518cf5038935a666ea792b297efdae6
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-07-25 17:00:35 +0800

    Update TagLink.java

--- src/main/java/com/github/dev/objectnotation/html/TagLink.java
+++ src/main/java/com/github/dev/objectnotation/html/TagLink.java
@@ -1,8 +1,33 @@
 package com.github.dev.objectnotation.html;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.github.dev.objectnotation.tree.Node;
 
+/**
+ * link
+ */
 class TagLink extends TagAbstractImpl {
+
+	private static final Set<String> ATTRIBUTES = new HashSet<>();
+
+	static {
+		ATTRIBUTES.add("href");
+		ATTRIBUTES.add("crossorigin");
+		ATTRIBUTES.add("rel");
+		ATTRIBUTES.add("as");
+		ATTRIBUTES.add("media");
+		ATTRIBUTES.add("hreflang");
+		ATTRIBUTES.add("type");
+		ATTRIBUTES.add("sizes");
+		ATTRIBUTES.add("imagesrcset");
+		ATTRIBUTES.add("imagesizes");
+		ATTRIBUTES.add("referrerpolicy");
+		ATTRIBUTES.add("integrity");
+		ATTRIBUTES.add("color");
+		ATTRIBUTES.add("disabled");
+	}
 
 	protected TagLink(Node node) {
 		super(node);
@@ -10,12 +35,7 @@ class TagLink extends TagAbstractImpl {
 
 	@Override
 	protected boolean isAttribute(String str) {
-		return true;
-	}
-
-	@Override
-	protected Tag createTag(String str, Node node) {
-		return null;
+		return ATTRIBUTES.contains(str);
 	}
 
 }
