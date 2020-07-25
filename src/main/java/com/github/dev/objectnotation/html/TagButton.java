370555581874064973b551031e4d7099ef4133d6
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-07-25 16:55:01 +0800

    Update TagButton.java

--- src/main/java/com/github/dev/objectnotation/html/TagButton.java
+++ src/main/java/com/github/dev/objectnotation/html/TagButton.java
@@ -1,8 +1,29 @@
 package com.github.dev.objectnotation.html;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.github.dev.objectnotation.tree.Node;
 
+/**
+ * button
+ */
 class TagButton extends TagAbstractImpl {
+
+	private static final Set<String> ATTRIBUTES = new HashSet<>();
+
+	static {
+		ATTRIBUTES.add("disabled");
+		ATTRIBUTES.add("form");
+		ATTRIBUTES.add("formaction");
+		ATTRIBUTES.add("formenctype");
+		ATTRIBUTES.add("formmethod");
+		ATTRIBUTES.add("formnovalidate");
+		ATTRIBUTES.add("formtarget");
+		ATTRIBUTES.add("name");
+		ATTRIBUTES.add("type");
+		ATTRIBUTES.add("value");
+	}
 
 	protected TagButton(Node node) {
 		super(node);
@@ -10,12 +31,7 @@ class TagButton extends TagAbstractImpl {
 
 	@Override
 	protected boolean isAttribute(String str) {
-		return true;
-	}
-
-	@Override
-	protected Tag createTag(String str, Node node) {
-		return null;
+		return ATTRIBUTES.contains(str);
 	}
 
 }
