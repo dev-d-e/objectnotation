7d8e2b3a46a9b6d5b474721c7ad544725618c1ec
dev0-0 <66547986+dev0-0@users.noreply.github.com>
2020-07-25 16:59:47 +0800

    Update TagInput.java

--- src/main/java/com/github/dev/objectnotation/html/TagInput.java
+++ src/main/java/com/github/dev/objectnotation/html/TagInput.java
@@ -1,8 +1,49 @@
 package com.github.dev.objectnotation.html;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.github.dev.objectnotation.tree.Node;
 
+/**
+ * input
+ */
 class TagInput extends TagAbstractImpl {
+
+	private static final Set<String> ATTRIBUTES = new HashSet<>();
+
+	static {
+		ATTRIBUTES.add("accept");
+		ATTRIBUTES.add("alt");
+		ATTRIBUTES.add("autocomplete");
+		ATTRIBUTES.add("checked");
+		ATTRIBUTES.add("dirname");
+		ATTRIBUTES.add("disabled");
+		ATTRIBUTES.add("form");
+		ATTRIBUTES.add("formaction");
+		ATTRIBUTES.add("formenctype");
+		ATTRIBUTES.add("formmethod ");
+		ATTRIBUTES.add("formnovalidate");
+		ATTRIBUTES.add("formtarget");
+		ATTRIBUTES.add("height");
+		ATTRIBUTES.add("list");
+		ATTRIBUTES.add("max");
+		ATTRIBUTES.add("maxlength ");
+		ATTRIBUTES.add("min");
+		ATTRIBUTES.add("minlength");
+		ATTRIBUTES.add("multiple");
+		ATTRIBUTES.add("name");
+		ATTRIBUTES.add("pattern");
+		ATTRIBUTES.add("placeholder");
+		ATTRIBUTES.add("readonly");
+		ATTRIBUTES.add("required");
+		ATTRIBUTES.add("size");
+		ATTRIBUTES.add("src");
+		ATTRIBUTES.add("step");
+		ATTRIBUTES.add("type");
+		ATTRIBUTES.add("value");
+		ATTRIBUTES.add("width");
+	}
 
 	protected TagInput(Node node) {
 		super(node);
@@ -10,12 +51,7 @@ class TagInput extends TagAbstractImpl {
 
 	@Override
 	protected boolean isAttribute(String str) {
-		return true;
-	}
-
-	@Override
-	protected Tag createTag(String str, Node node) {
-		return null;
+		return ATTRIBUTES.contains(str);
 	}
 
 }
