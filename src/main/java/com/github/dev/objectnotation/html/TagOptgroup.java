79c5c6edfd4528c314131edd0ea9b06f0ffa0047
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-07-26 14:06:14 +0800

    Create TagOptgroup.java

---
+++ src/main/java/com/github/dev/objectnotation/html/TagOptgroup.java
@@ -0,0 +1,29 @@
+package com.github.dev.objectnotation.html;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.github.dev.objectnotation.tree.Node;
+
+/**
+ * optgroup
+ */
+class TagOptgroup extends TagAbstractImpl {
+
+	private static final Set<String> ATTRIBUTES = new HashSet<>();
+
+	static {
+		ATTRIBUTES.add("disabled");
+		ATTRIBUTES.add("label");
+	}
+
+	protected TagOptgroup(Node node) {
+		super(node);
+	}
+
+	@Override
+	protected boolean isAttribute(String str) {
+		return ATTRIBUTES.contains(str);
+	}
+
+}
