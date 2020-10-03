c333d82a54baa5cd5052fd0d987db1323a379bec
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-10-03 20:41:12 +0800

    Create ParseValue.java

---
+++ src/main/java/com/github/dev/objectnotation/ParseValue.java
@@ -0,0 +1,33 @@
+package com.github.dev.objectnotation;
+
+/**
+ * Parse value.
+ */
+final class ParseValue implements IntToFunction {
+
+	private final ValueParser parser;
+
+	ParseValue(ValueParser parser) {
+		this.parser = parser;
+	}
+
+	@Override
+	public IntToFunction apply(int i) {
+		if (i == -1) {
+			parser.valueConsumer.accept(-1);
+			return this;
+		}
+		char c = (char) i;
+		if (c == '\\') {
+			return parser.backslash;
+		} else if (c == '&') {
+			return parser.quote;
+		} else if (c == '[') {
+			parser.valueConsumer.opt(1);
+			return parser.array;
+		}
+		parser.valueConsumer.accept(i);
+		return this;
+	}
+
+}
