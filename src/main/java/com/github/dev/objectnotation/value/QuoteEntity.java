794e5d67f28e07d2ec632abee35d15f003b976db
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-10-15 21:36:19 +0800

    Create QuoteEntity.java

---
+++ src/main/java/com/github/dev/objectnotation/value/QuoteEntity.java
@@ -0,0 +1,13 @@
+package com.github.dev.objectnotation.value;
+
+/**
+ * Quote type entity.
+ */
+public interface QuoteEntity extends Entity {
+
+	/**
+	 * Returns value.
+	 */
+	String getValue();
+
+}
