bb50f98cc8633ddf6bf1eabb28079f554154bbcf
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-10-15 21:33:11 +0800

    Update Entity.java

--- src/main/java/com/github/dev/objectnotation/value/Entity.java
+++ src/main/java/com/github/dev/objectnotation/value/Entity.java
@@ -5,4 +5,23 @@ package com.github.dev.objectnotation.value;
  */
 public interface Entity extends Cloneable {
 
+	/**
+	 * Performs this operation on the given argument.
+	 *
+	 * @param c the input argument
+	 */
+	Entity accept(char c);
+
+	/**
+	 * Finish the operation.
+	 */
+	void finish();
+
+	/**
+	 * Returns true if this entity contains no value.
+	 *
+	 * @return true if this entity contains no value
+	 */
+	boolean isEmpty();
+
 }
