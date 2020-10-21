b36bd319771494655304538edd1fbca3dbc9aeaa
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-10-21 16:31:26 +0800

    Update DocumentFactory.java

--- src/main/java/com/github/dev/objectnotation/tree/DocumentFactory.java
+++ src/main/java/com/github/dev/objectnotation/tree/DocumentFactory.java
@@ -2,9 +2,8 @@ package com.github.dev.objectnotation.tree;
 
 import java.util.Objects;
 
-import com.github.dev.objectnotation.value.ArrayEntity;
 import com.github.dev.objectnotation.value.Entity;
-import com.github.dev.objectnotation.value.EntityFactory;
+import com.github.dev.objectnotation.value.QuoteEntity;
 
 /**
  * Document factory.
@@ -18,75 +17,69 @@ public class DocumentFactory {
 		return document;
 	}
 
-	public void addBranch(int offset, String key) {
-		Objects.requireNonNull(key);
-		if (offset < 0) {
-			return;
-		}
+	private void add(int offset, Node node) {
 		if (offset == 0) {
-			Node node = new BranchNodeImpl(offset, key);
 			document.add(node);
 			last = node;
 			return;
 		}
-		Node node = new BranchNodeImpl(offset, key);
 		if (offset == (last.getOffset() + 1)) {
-			node.setParent(last);
 			last.add(node);
 		} else {
-			Node p = last;
-			while (p != null && offset != p.getOffset()) {
-				p = p.getParent();
-			}
+			Node p = last.getNodeByOffset(offset);
 			if (p == null) {
 				return;
 			}
-			node.setParent(p.getParent());
 			p.getParent().add(node);
 		}
 		last = node;
 	}
 
-	public void addLeaf(int offset, String key, Entity entity) {
+	private void addBranch(int offset, String key) {
+		Objects.requireNonNull(key);
+		if (offset < 0) {
+			return;
+		}
+		add(offset, new BranchNodeImpl(offset, key));
+	}
+
+	private void addLeaf(int offset, String key, Entity entity) {
 		Objects.requireNonNull(key);
 		if (offset < 0) {
 			return;
 		}
 		if (last.getOffset() == offset && last.getKey().equals(key)) {
-			Entity lastEntity = last.getEntity();
-			if (lastEntity instanceof ArrayEntity) {
-				((ArrayEntity) lastEntity).add(entity);
-			} else {
-				ArrayEntity arrayEntity = EntityFactory.createArrayEntity();
-				arrayEntity.add(lastEntity);
-				arrayEntity.add(entity);
-				last.setEntity(arrayEntity);
-			}
+			last.addEntity(entity);
 			return;
 		}
-		if (offset == 0) {
-			Node node = new LeafNodeImpl(offset, key, entity);
-			document.add(node);
+		add(offset, new LeafNodeImpl(offset, key, entity));
+	}
+
+	private void addNode(int offset, String key, Node node) {
+		Objects.requireNonNull(key);
+		if (offset < 0 || node == null) {
+			return;
+		}
+		if (node.isBranch()) {
+			addBranch(offset, key);
+			node.iterator().forEachRemaining(o -> last.add(o));
+			while (node != null && node.size() > 0) {
+				node = node.node(node.size() - 1);
+			}
 			last = node;
-			return;
-		}
-		Node node = new LeafNodeImpl(offset, key, entity);
-		if (offset == (last.getOffset() + 1)) {
-			node.setParent(last);
-			last.add(node);
 		} else {
-			Node p = last;
-			while (p != null && offset != p.getOffset()) {
-				p = p.getParent();
-			}
-			if (p == null) {
-				return;
-			}
-			node.setParent(p.getParent());
-			p.getParent().add(node);
+			addLeaf(offset, key, node.getEntity());
 		}
-		document.addMapping(node);
-		last = node;
+	}
+
+	public void addNode(int offset, String key, Entity entity) {
+		if (entity.isEmpty()) {
+			addBranch(offset, key);
+		} else if (entity instanceof QuoteEntity) {
+			addNode(offset, key, document.getNode(((QuoteEntity) entity).getValue()));
+		} else {
+			addLeaf(offset, key, entity);
+		}
 	}
 
 }
