c796bfb0aec7d3aec8fa049292f651b957c8bf58
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-11-06 20:43:13 +0800

    Update TypeConsumer.java

--- src/main/java/com/github/dev/objectnotation/TypeConsumer.java
+++ src/main/java/com/github/dev/objectnotation/TypeConsumer.java
@@ -1,19 +1,8 @@
 package com.github.dev.objectnotation;
 
-import com.github.dev.objectnotation.value.Entity;
-
 /**
  * Type consumer.
  */
 interface TypeConsumer {
-
-	void setEntity(Entity entity);
-
-	/**
-	 * Performs this operation on the given argument.
-	 *
-	 * @param i the input argument
-	 */
-	void accept(int i);
 
 }
