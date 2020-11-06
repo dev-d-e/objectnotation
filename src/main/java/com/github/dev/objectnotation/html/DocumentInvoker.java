5c90929ab22aaee65390efe49246b42cae1559a2
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-11-06 20:44:58 +0800

    Update DocumentInvoker.java

--- src/main/java/com/github/dev/objectnotation/html/DocumentInvoker.java
+++ src/main/java/com/github/dev/objectnotation/html/DocumentInvoker.java
@@ -1,7 +1,5 @@
 package com.github.dev.objectnotation.html;
 
-import com.github.dev.objectnotation.tree.Document;
-
 /**
  * Invoker provides static methods.
  */
@@ -11,11 +9,6 @@ public final class DocumentInvoker {
 	 * Suppress default constructor, ensuring non-instantiability.
 	 */
 	private DocumentInvoker() {
-	}
-
-	public static Document accept(CharSequence charSequence) {
-		DocumentResolver treeResolver = new DocumentResolver(charSequence);
-		return treeResolver.getDocument();
 	}
 
 }
