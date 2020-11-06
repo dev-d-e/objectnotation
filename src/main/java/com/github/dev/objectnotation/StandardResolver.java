60054a72eb254d40ec52971609ce7056da8ccb0a
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-11-06 20:43:28 +0800

    Update StandardResolver.java

--- src/main/java/com/github/dev/objectnotation/StandardResolver.java
+++ src/main/java/com/github/dev/objectnotation/StandardResolver.java
@@ -1,5 +1,10 @@
 package com.github.dev.objectnotation;
 
+import com.github.dev.objectnotation.tree.Document;
+import com.github.dev.objectnotation.tree.DocumentFactory;
+import com.github.dev.objectnotation.value.Entity;
+import com.github.dev.objectnotation.value.EntityFactory;
+
 /**
  * Resolve key and value pair.
  */
@@ -8,21 +13,49 @@ class StandardResolver {
 	private final Parser parser;
 	private final ValueParser valueParser;
 
+	private final DocumentFactory documentFactory = new DocumentFactory();
+
+	private int curOffset = -1;
+	private String curKey;
+	private Entity curEntity = EntityFactory.createPrimitiveTypeEntity();
+	private boolean negative;
+
 	/**
-	 * Constructs a {@code Parser} and a {@code ValueParser} with two consumers.
-	 * 
-	 * @param keyConsumer   the consumer of the key.
-	 * @param valueConsumer the consumer of the value.
+	 * Constructs a {@code Parser} and a {@code ValueParser}.
 	 */
-	StandardResolver(IntStringConsumer keyConsumer, TypeConsumer valueConsumer) {
-		valueParser = new ValueParser(valueConsumer);
-		parser = new Parser(keyConsumer, i -> {
+	StandardResolver() {
+		valueParser = new ValueParser(entity -> {
+			curEntity = entity;
+		}, i -> {
+			if (i == -1) {
+				if (negative) {
+					return;
+				}
+				curEntity.finish();
+				if (documentFactory.notEqualsLast(curOffset, curKey, curEntity)) {
+					documentFactory.addNode(curOffset, curKey, curEntity);
+				}
+				curEntity = EntityFactory.createPrimitiveTypeEntity();
+				negative = true;
+			} else {
+				curEntity.accept((char) i);
+			}
+		});
+		parser = new Parser((i, s) -> {
+			curOffset = i;
+			curKey = s;
+			negative = false;
+		}, i -> {
 			valueParser.apply(i);
 		});
 	}
 
 	public void apply(int i) {
 		parser.apply(i);
+	}
+
+	public Document getDocument() {
+		return documentFactory.getDocument();
 	}
 
 }
