f6c18de7576cf7d7f20ed49807d0b578e29aba0b
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-11-20 15:48:14 +0800

    Update TagBase.java

--- src/main/java/com/github/dev/objectnotation/html/TagBase.java
+++ src/main/java/com/github/dev/objectnotation/html/TagBase.java
@@ -3,8 +3,6 @@ package com.github.dev.objectnotation.html;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.github.dev.objectnotation.tree.Node;
-
 /**
  * base
  */
@@ -17,12 +15,12 @@ class TagBase extends TagAbstractImpl {
 		ATTRIBUTES.add("target");
 	}
 
-	protected TagBase(Node node) {
-		super(node);
+	public TagBase() {
+		super("base");
 	}
 
 	@Override
-	protected boolean isAttribute(String str) {
+	public boolean isAttribute(String str) {
 		return ATTRIBUTES.contains(str);
 	}
 
