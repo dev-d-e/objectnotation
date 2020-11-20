3500e28a2ddefb5d7d006e9e540864498fcefd03
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-11-20 15:53:11 +0800

    Update TagIframe.java

--- src/main/java/com/github/dev/objectnotation/html/TagIframe.java
+++ src/main/java/com/github/dev/objectnotation/html/TagIframe.java
@@ -3,8 +3,6 @@ package com.github.dev.objectnotation.html;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.github.dev.objectnotation.tree.Node;
-
 /**
  * iframe
  */
@@ -26,12 +24,12 @@ class TagIframe extends TagAbstractImpl {
 		ATTRIBUTES.add("loading");
 	}
 
-	protected TagIframe(Node node) {
-		super(node);
+	public TagIframe() {
+		super("iframe");
 	}
 
 	@Override
-	protected boolean isAttribute(String str) {
+	public boolean isAttribute(String str) {
 		return ATTRIBUTES.contains(str);
 	}
 
