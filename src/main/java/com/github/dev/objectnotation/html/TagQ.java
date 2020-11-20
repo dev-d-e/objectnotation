b0b6014a5d27b1a90c50e528c6896b89e1326dba
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-11-20 15:57:17 +0800

    Update TagQ.java

--- src/main/java/com/github/dev/objectnotation/html/TagQ.java
+++ src/main/java/com/github/dev/objectnotation/html/TagQ.java
@@ -3,8 +3,6 @@ package com.github.dev.objectnotation.html;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.github.dev.objectnotation.tree.Node;
-
 /**
  * q
  */
@@ -16,12 +14,12 @@ class TagQ extends TagAbstractImpl {
 		ATTRIBUTES.add("cite");
 	}
 
-	protected TagQ(Node node) {
-		super(node);
+	public TagQ() {
+		super("q");
 	}
 
 	@Override
-	protected boolean isAttribute(String str) {
+	public boolean isAttribute(String str) {
 		return ATTRIBUTES.contains(str);
 	}
 
