e08bc0aab1647b04e5437a94cf200b2eefa5e0d6
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-11-20 15:58:09 +0800

    Update TagSlot.java

--- src/main/java/com/github/dev/objectnotation/html/TagSlot.java
+++ src/main/java/com/github/dev/objectnotation/html/TagSlot.java
@@ -3,8 +3,6 @@ package com.github.dev.objectnotation.html;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.github.dev.objectnotation.tree.Node;
-
 /**
  * slot
  */
@@ -16,12 +14,12 @@ class TagSlot extends TagAbstractImpl {
 		ATTRIBUTES.add("name");
 	}
 
-	protected TagSlot(Node node) {
-		super(node);
+	public TagSlot() {
+		super("slot");
 	}
 
 	@Override
-	protected boolean isAttribute(String str) {
+	public boolean isAttribute(String str) {
 		return ATTRIBUTES.contains(str);
 	}
 
