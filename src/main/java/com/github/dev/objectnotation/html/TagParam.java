bd5297f940b152601d4f68f8abff9eed6d00328f
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-11-20 15:57:02 +0800

    Update TagParam.java

--- src/main/java/com/github/dev/objectnotation/html/TagParam.java
+++ src/main/java/com/github/dev/objectnotation/html/TagParam.java
@@ -3,8 +3,6 @@ package com.github.dev.objectnotation.html;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.github.dev.objectnotation.tree.Node;
-
 /**
  * param
  */
@@ -17,12 +15,12 @@ class TagParam extends TagAbstractImpl {
 		ATTRIBUTES.add("value");
 	}
 
-	protected TagParam(Node node) {
-		super(node);
+	public TagParam() {
+		super("param");
 	}
 
 	@Override
-	protected boolean isAttribute(String str) {
+	public boolean isAttribute(String str) {
 		return ATTRIBUTES.contains(str);
 	}
 
