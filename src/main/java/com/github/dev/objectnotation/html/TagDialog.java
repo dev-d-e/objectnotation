7b1f035da29f23547e36f3b2edcd312020fc9dd7
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-11-20 15:51:10 +0800

    Update TagDialog.java

--- src/main/java/com/github/dev/objectnotation/html/TagDialog.java
+++ src/main/java/com/github/dev/objectnotation/html/TagDialog.java
@@ -3,8 +3,6 @@ package com.github.dev.objectnotation.html;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.github.dev.objectnotation.tree.Node;
-
 /**
  * dialog
  */
@@ -16,12 +14,12 @@ class TagDialog extends TagAbstractImpl {
 		ATTRIBUTES.add("open");
 	}
 
-	protected TagDialog(Node node) {
-		super(node);
+	public TagDialog() {
+		super("dialog");
 	}
 
 	@Override
-	protected boolean isAttribute(String str) {
+	public boolean isAttribute(String str) {
 		return ATTRIBUTES.contains(str);
 	}
 
