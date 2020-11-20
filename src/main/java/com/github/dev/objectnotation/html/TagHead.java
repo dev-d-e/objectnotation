c4594cc277ba185ee7b2e1ec774c95b736587403
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-11-20 15:52:49 +0800

    Update TagHead.java

--- src/main/java/com/github/dev/objectnotation/html/TagHead.java
+++ src/main/java/com/github/dev/objectnotation/html/TagHead.java
@@ -1,18 +1,16 @@
 package com.github.dev.objectnotation.html;
 
-import com.github.dev.objectnotation.tree.Node;
-
 /**
  * head
  */
 class TagHead extends TagAbstractImpl {
 
-	protected TagHead(Node node) {
-		super(node);
+	public TagHead() {
+		super("head");
 	}
 
 	@Override
-	protected boolean isAttribute(String str) {
+	public boolean isAttribute(String str) {
 		return false;
 	}
 
