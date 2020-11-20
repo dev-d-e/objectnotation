60b18407d12a3a894f04b80e7f6e06cb4efbf63b
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-11-20 15:46:42 +0800

    Update TagAbstractImpl.java

--- src/main/java/com/github/dev/objectnotation/html/TagAbstractImpl.java
+++ src/main/java/com/github/dev/objectnotation/html/TagAbstractImpl.java
@@ -4,35 +4,24 @@ package com.github.dev.objectnotation.html;
  * Abstract class for {@code Tag}.
  */
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import com.github.dev.objectnotation.tree.BranchNode;
-import com.github.dev.objectnotation.tree.LeafNode;
-import com.github.dev.objectnotation.tree.Node;
-import com.github.dev.objectnotation.value.Entity;
-import com.github.dev.objectnotation.value.PrimitiveTypeEntity;
-
 abstract class TagAbstractImpl implements Tag {
 
-	private String name;
+	private final String name;
 
-	protected Map<String, String> attributeMap = new LinkedHashMap<>();
+	protected final Map<String, String> attributeMap = new LinkedHashMap<>();
 
-	protected List<Tag> tags = new ArrayList<>();
+	protected String value;
 
-	protected StringBuilder value = new StringBuilder();
+	protected final List<Tag> tags = new ArrayList<>();
 
-	protected TagAbstractImpl(Node node) {
-		name = node.getKey();
-		if (node instanceof BranchNode) {
-			setNode(((BranchNode) node).toArray());
-		}
-		if (node instanceof LeafNode) {
-			setEntity(((LeafNode) node).getEntity());
-		}
+	public TagAbstractImpl(String name) {
+		this.name = name;
 	}
 
 	@Override
@@ -41,286 +30,258 @@ abstract class TagAbstractImpl implements Tag {
 	}
 
 	@Override
-	public Tag setTagName(String tagName) {
-		name = tagName;
-		return this;
-	}
-
-	@Override
-	public Tag setNode(Node[] nodes) {
-		if (nodes == null) {
-			return this;
-		}
-		for (Node node : nodes) {
-			String k = node.getKey();
-			if (Attributes.isAttr(k.toLowerCase()) || isAttribute(k.toLowerCase())) {
-				if (node instanceof LeafNode) {
-					Entity attributeValue = ((LeafNode) node).getEntity();
-					if (attributeValue != null && attributeValue instanceof PrimitiveTypeEntity) {
-						put(k, ((PrimitiveTypeEntity) attributeValue).getValue());
-					}
-				}
-			} else {
-				Tag tag = createTag(k.toLowerCase(), node);
-				if (tag != null) {
-					tags.add(tag);
-				}
-			}
-		}
-		return this;
-	}
-
-	private void put(String key, String value) {
-		String s = attributeMap.get(key);
+	public Tag setAttribute(String attrName, String attrValue) {
+		String s = attributeMap.get(attrName);
 		if (s != null && s.length() > 0) {
-			if (value == null || value.isEmpty()) {
-				return;
+			if (attrValue == null || attrValue.isEmpty()) {
+				return this;
 			}
-			value = new StringBuilder(s).append(' ').append(value).toString();
+			attrValue = new StringBuilder(s).append(' ').append(attrValue).toString();
 		}
-		attributeMap.put(key, value);
+		attributeMap.put(attrName, attrValue);
+		return this;
 	}
 
-	protected abstract boolean isAttribute(String str);
-
-	protected Tag createTag(String str, Node node) {
-		switch (str) {
-		case "a":
-			return new TagA(node);
-		case "abbr":
-			return new TagCommon(node);
-		case "address":
-			return new TagCommon(node);
-		case "area":
-			return new TagArea(node);
-		case "article":
-			return new TagCommon(node);
-		case "aside":
-			return new TagCommon(node);
-		case "audio":
-			return new TagAudio(node);
-		case "b":
-			return new TagCommon(node);
-		case "base":
-			return new TagBase(node);
-		case "bdi":
-			return new TagCommon(node);
-		case "bdo":
-			return new TagCommon(node);
-		case "blockquote":
-			return new TagBlockquote(node);
-		case "br":
-			return new TagCommon(node);
-		case "button":
-			return new TagButton(node);
-		case "canvas":
-			return new TagCanvas(node);
-		case "caption":
-			return new TagCommon(node);
-		case "cite":
-			return new TagCommon(node);
-		case "code":
-			return new TagCommon(node);
-		case "col":
-			return new TagCol(node);
-		case "colgroup":
-			return new TagCol(node);
-		case "data":
-			return new TagData(node);
-		case "datalist":
-			return new TagCommon(node);
-		case "dd":
-			return new TagCommon(node);
-		case "del":
-			return new TagDel(node);
-		case "details":
-			return new TagDetails(node);
-		case "dfn":
-			return new TagCommon(node);
-		case "dialog":
-			return new TagDialog(node);
-		case "div":
-			return new TagCommon(node);
-		case "dl":
-			return new TagCommon(node);
-		case "dt":
-			return new TagCommon(node);
-		case "em":
-			return new TagCommon(node);
-		case "embed":
-			return new TagEmbed(node);
-		case "fieldset":
-			return new TagFieldset(node);
-		case "figcaption":
-			return new TagCommon(node);
-		case "figure":
-			return new TagCommon(node);
-		case "footer":
-			return new TagCommon(node);
-		case "form":
-			return new TagForm(node);
-		case "h1":
-			return new TagCommon(node);
-		case "h2":
-			return new TagCommon(node);
-		case "h3":
-			return new TagCommon(node);
-		case "h4":
-			return new TagCommon(node);
-		case "h5":
-			return new TagCommon(node);
-		case "h6":
-			return new TagCommon(node);
-		case "header":
-			return new TagCommon(node);
-		case "hgroup":
-			return new TagCommon(node);
-		case "hr":
-			return new TagCommon(node);
-		case "i":
-			return new TagCommon(node);
-		case "iframe ":
-			return new TagIframe(node);
-		case "img":
-			return new TagImg(node);
-		case "input":
-			return new TagInput(node);
-		case "ins":
-			return new TagIns(node);
-		case "kbd":
-			return new TagCommon(node);
-		case "label":
-			return new TagLabel(node);
-		case "legend":
-			return new TagCommon(node);
-		case "li":
-			return new TagLi(node);
-		case "link":
-			return new TagLink(node);
-		case "main":
-			return new TagCommon(node);
-		case "map":
-			return new TagMap(node);
-		case "mark":
-			return new TagCommon(node);
-		case "math":
-			return new TagCommon(node);
-		case "menu":
-			return new TagCommon(node);
-		case "meta":
-			return new TagMeta(node);
-		case "meter":
-			return new TagMeter(node);
-		case "nav":
-			return new TagCommon(node);
-		case "noscript":
-			return new TagCommon(node);
-		case "object":
-			return new TagObject(node);
-		case "ol":
-			return new TagOl(node);
-		case "optgroup":
-			return new TagOptgroup(node);
-		case "option":
-			return new TagOption(node);
-		case "output":
-			return new TagOutput(node);
-		case "p":
-			return new TagCommon(node);
-		case "param":
-			return new TagParam(node);
-		case "picture":
-			return new TagCommon(node);
-		case "pre":
-			return new TagCommon(node);
-		case "progress":
-			return new TagProgress(node);
-		case "q":
-			return new TagQ(node);
-		case "rp":
-			return new TagCommon(node);
-		case "rt":
-			return new TagCommon(node);
-		case "ruby":
-			return new TagCommon(node);
-		case "s":
-			return new TagCommon(node);
-		case "samp":
-			return new TagCommon(node);
-		case "script":
-			return new TagScript(node);
-		case "section":
-			return new TagCommon(node);
-		case "select":
-			return new TagSelect(node);
-		case "slot":
-			return new TagSlot(node);
-		case "small":
-			return new TagCommon(node);
-		case "source":
-			return new TagSource(node);
-		case "span":
-			return new TagCommon(node);
-		case "strong":
-			return new TagCommon(node);
-		case "style":
-			return new TagStyle(node);
-		case "sub":
-			return new TagCommon(node);
-		case "summary":
-			return new TagCommon(node);
-		case "sup":
-			return new TagCommon(node);
-		case "svg":
-			return new TagCommon(node);
-		case "table":
-			return new TagCommon(node);
-		case "tbody":
-			return new TagCommon(node);
-		case "td":
-			return new TagTd(node);
-		case "template":
-			return new TagCommon(node);
-		case "textarea":
-			return new TagTextarea(node);
-		case "tfoot":
-			return new TagCommon(node);
-		case "th":
-			return new TagTh(node);
-		case "thead":
-			return new TagCommon(node);
-		case "time":
-			return new TagTime(node);
-		case "title":
-			return new TagCommon(node);
-		case "tr":
-			return new TagCommon(node);
-		case "track":
-			return new TagTrack(node);
-		case "u":
-			return new TagCommon(node);
-		case "ul":
-			return new TagCommon(node);
-		case "var":
-			return new TagCommon(node);
-		case "video":
-			return new TagVideo(node);
-		case "wbr":
-			return new TagCommon(node);
-		default:
-			return new TagCommon(node);
-		}
-	}
+	public abstract boolean isAttribute(String str);
 
 	@Override
-	public Tag setEntity(Entity entity) {
-		if (entity == null) {
-			return this;
+	public Tag createTag(String name) {
+		Tag t = createTag0(name);
+		tags.add(t);
+		return t;
+	}
+
+	private Tag createTag0(String name) {
+		switch (name) {
+		case "a":
+			return new TagA();
+		case "abbr":
+			return new TagCommon(name);
+		case "address":
+			return new TagCommon(name);
+		case "area":
+			return new TagArea();
+		case "article":
+			return new TagCommon(name);
+		case "aside":
+			return new TagCommon(name);
+		case "audio":
+			return new TagAudio();
+		case "b":
+			return new TagCommon(name);
+		case "base":
+			return new TagBase();
+		case "bdi":
+			return new TagCommon(name);
+		case "bdo":
+			return new TagCommon(name);
+		case "blockquote":
+			return new TagBlockquote();
+		case "body":
+			return new TagBody();
+		case "br":
+			return new TagCommon(name);
+		case "button":
+			return new TagButton();
+		case "canvas":
+			return new TagCanvas();
+		case "caption":
+			return new TagCommon(name);
+		case "cite":
+			return new TagCommon(name);
+		case "code":
+			return new TagCommon(name);
+		case "col":
+			return new TagCol(name);
+		case "colgroup":
+			return new TagCol(name);
+		case "data":
+			return new TagData();
+		case "datalist":
+			return new TagCommon(name);
+		case "dd":
+			return new TagCommon(name);
+		case "del":
+			return new TagDel();
+		case "details":
+			return new TagDetails();
+		case "dfn":
+			return new TagCommon(name);
+		case "dialog":
+			return new TagDialog();
+		case "div":
+			return new TagCommon(name);
+		case "dl":
+			return new TagCommon(name);
+		case "dt":
+			return new TagCommon(name);
+		case "em":
+			return new TagCommon(name);
+		case "embed":
+			return new TagEmbed();
+		case "fieldset":
+			return new TagFieldset();
+		case "figcaption":
+			return new TagCommon(name);
+		case "figure":
+			return new TagCommon(name);
+		case "footer":
+			return new TagCommon(name);
+		case "form":
+			return new TagForm();
+		case "h1":
+			return new TagCommon(name);
+		case "h2":
+			return new TagCommon(name);
+		case "h3":
+			return new TagCommon(name);
+		case "h4":
+			return new TagCommon(name);
+		case "h5":
+			return new TagCommon(name);
+		case "h6":
+			return new TagCommon(name);
+		case "header":
+			return new TagCommon(name);
+		case "hgroup":
+			return new TagCommon(name);
+		case "hr":
+			return new TagCommon(name);
+		case "head":
+			return new TagHead();
+		case "i":
+			return new TagCommon(name);
+		case "iframe":
+			return new TagIframe();
+		case "img":
+			return new TagImg();
+		case "input":
+			return new TagInput();
+		case "ins":
+			return new TagIns();
+		case "kbd":
+			return new TagCommon(name);
+		case "label":
+			return new TagLabel();
+		case "legend":
+			return new TagCommon(name);
+		case "li":
+			return new TagLi();
+		case "link":
+			return new TagLink();
+		case "main":
+			return new TagCommon(name);
+		case "map":
+			return new TagMap();
+		case "mark":
+			return new TagCommon(name);
+		case "math":
+			return new TagCommon(name);
+		case "menu":
+			return new TagCommon(name);
+		case "meta":
+			return new TagMeta();
+		case "meter":
+			return new TagMeter();
+		case "nav":
+			return new TagCommon(name);
+		case "noscript":
+			return new TagCommon(name);
+		case "object":
+			return new TagObject();
+		case "ol":
+			return new TagOl();
+		case "optgroup":
+			return new TagOptgroup();
+		case "option":
+			return new TagOption();
+		case "output":
+			return new TagOutput();
+		case "p":
+			return new TagCommon(name);
+		case "param":
+			return new TagParam();
+		case "picture":
+			return new TagCommon(name);
+		case "pre":
+			return new TagCommon(name);
+		case "progress":
+			return new TagProgress();
+		case "q":
+			return new TagQ();
+		case "rp":
+			return new TagCommon(name);
+		case "rt":
+			return new TagCommon(name);
+		case "ruby":
+			return new TagCommon(name);
+		case "s":
+			return new TagCommon(name);
+		case "samp":
+			return new TagCommon(name);
+		case "script":
+			return new TagScript();
+		case "section":
+			return new TagCommon(name);
+		case "select":
+			return new TagSelect();
+		case "slot":
+			return new TagSlot();
+		case "small":
+			return new TagCommon(name);
+		case "source":
+			return new TagSource();
+		case "span":
+			return new TagCommon(name);
+		case "strong":
+			return new TagCommon(name);
+		case "style":
+			return new TagStyle();
+		case "sub":
+			return new TagCommon(name);
+		case "summary":
+			return new TagCommon(name);
+		case "sup":
+			return new TagCommon(name);
+		case "svg":
+			return new TagCommon(name);
+		case "table":
+			return new TagCommon(name);
+		case "tbody":
+			return new TagCommon(name);
+		case "td":
+			return new TagTd();
+		case "template":
+			return new TagCommon(name);
+		case "textarea":
+			return new TagTextarea();
+		case "tfoot":
+			return new TagCommon(name);
+		case "th":
+			return new TagTh();
+		case "thead":
+			return new TagCommon(name);
+		case "time":
+			return new TagTime();
+		case "title":
+			return new TagCommon(name);
+		case "tr":
+			return new TagCommon(name);
+		case "track":
+			return new TagTrack();
+		case "u":
+			return new TagCommon(name);
+		case "ul":
+			return new TagCommon(name);
+		case "var":
+			return new TagCommon(name);
+		case "video":
+			return new TagVideo();
+		case "wbr":
+			return new TagCommon(name);
+		default:
+			return new TagCommon(name);
 		}
-		if (entity instanceof PrimitiveTypeEntity) {
-			value.append(((PrimitiveTypeEntity) entity).getValue());
-		}
-		return this;
 	}
 
 	@Override
@@ -330,12 +291,18 @@ abstract class TagAbstractImpl implements Tag {
 
 	@Override
 	public Map<String, String> getAttributeMap() {
-		return attributeMap;
+		return Collections.unmodifiableMap(attributeMap);
 	}
 
 	@Override
 	public String getTagValue() {
-		return value.length() == 0 ? "" : value.toString();
+		return value == null ? "" : value;
+	}
+
+	@Override
+	public Tag setTagValue(String tagValue) {
+		value = tagValue;
+		return this;
 	}
 
 	@Override
