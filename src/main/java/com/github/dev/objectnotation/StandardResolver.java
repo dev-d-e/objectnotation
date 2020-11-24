7ae4532cd3cf04b73548995749b7da10bbafdd6a
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-11-24 16:00:24 +0800

    Update StandardResolver.java

--- src/main/java/com/github/dev/objectnotation/StandardResolver.java
+++ src/main/java/com/github/dev/objectnotation/StandardResolver.java
@@ -2,8 +2,6 @@ package com.github.dev.objectnotation;
 
 import com.github.dev.objectnotation.tree.Document;
 import com.github.dev.objectnotation.tree.DocumentFactory;
-import com.github.dev.objectnotation.value.Entity;
-import com.github.dev.objectnotation.value.EntityFactory;
 
 /**
  * Resolve key and value pair.
@@ -17,32 +15,29 @@ class StandardResolver {
 
 	private int curOffset = -1;
 	private String curKey;
-	private Entity curEntity = EntityFactory.createPrimitiveTypeEntity();
-	private boolean negative;
+	private StringBuilder builder;
 
 	/**
 	 * Constructs a {@code Parser} and a {@code ValueParser}.
 	 */
 	StandardResolver() {
-		valueParser = new ValueParser(entity -> {
-			curEntity = entity;
-		}, i -> {
-			if (i == -1) {
-				if (negative) {
-					return;
+		valueParser = new ValueParser(i -> {
+			if (builder != null) {
+				if (i == -1) {
+					documentFactory.addNode(curOffset, curKey, builder);
+					builder = null;
+				} else if (i == -2) {
+					documentFactory.addNode(curOffset, curKey, builder);
+					builder = new StringBuilder();
+				} else {
+					builder.append((char) i);
 				}
-				curEntity.finish();
-				documentFactory.addNode(curOffset, curKey, curEntity);
-				curEntity = EntityFactory.createPrimitiveTypeEntity();
-				negative = true;
-			} else {
-				curEntity.accept((char) i);
 			}
 		});
 		parser = new Parser((i, s) -> {
 			curOffset = i;
 			curKey = s;
-			negative = false;
+			builder = new StringBuilder();
 		}, i -> {
 			valueParser.apply(i);
 		});
