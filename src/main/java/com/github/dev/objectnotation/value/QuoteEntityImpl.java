fe2a6a91d7aefc9c338d131502be3849b13841e5
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-11-24 15:56:47 +0800

    Update QuoteEntityImpl.java

--- src/main/java/com/github/dev/objectnotation/value/QuoteEntityImpl.java
+++ src/main/java/com/github/dev/objectnotation/value/QuoteEntityImpl.java
@@ -7,24 +7,6 @@ class QuoteEntityImpl implements QuoteEntity {
 
 	private String value;
 
-	private StringBuilder builder = new StringBuilder();
-
-	@Override
-	public QuoteEntity accept(char c) {
-		builder.append(c);
-		return this;
-	}
-
-	@Override
-	public void finish() {
-		value = builder.toString();
-	}
-
-	@Override
-	public boolean isEmpty() {
-		return value == null || value.isEmpty();
-	}
-
 	@Override
 	public String getValue() {
 		return value;
