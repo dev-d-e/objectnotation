59fd440ff60f11a8163d266157124fd486101a00
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-12-04 21:29:56 +0800

    Update HtmlConverter.java

--- src/main/java/com/github/dev/objectnotation/html/HtmlConverter.java
+++ src/main/java/com/github/dev/objectnotation/html/HtmlConverter.java
@@ -1,5 +1,7 @@
 package com.github.dev.objectnotation.html;
 
+import java.util.List;
+
 import com.github.dev.objectnotation.DirectTextInvoker;
 import com.github.dev.objectnotation.tree.BranchNode;
 import com.github.dev.objectnotation.tree.Document;
@@ -14,19 +16,18 @@ public class HtmlConverter {
 
 	public static String convert(CharSequence charSequence) {
 		Document document = DirectTextInvoker.accept(charSequence);
-		TagHtml t = convert(document);
+		TagImpl t = convert(document);
 		return t.asHtml();
 	}
 
-	private static TagHtml convert(Document doc) {
-		TagHtml t = new TagHtml();
-		Node[] nodes = doc.nodes();
-		if (nodes == null || nodes.length == 0) {
-			return t;
+	private static TagImpl convert(Document doc) {
+		List<Node> nodes = doc.nodes();
+		if (nodes == null || nodes.isEmpty()) {
+			return TagImpl.EMPTY;
 		}
 		Node node = null;
-		if (nodes.length == 1) {
-			node = nodes[0];
+		if (nodes.size() == 1) {
+			node = nodes.get(0);
 		} else {
 			for (Node o : nodes) {
 				if ("html".equalsIgnoreCase(o.getKey())) {
@@ -36,31 +37,40 @@ public class HtmlConverter {
 			}
 		}
 		if (node == null) {
-			return t;
+			return TagImpl.EMPTY;
 		}
+		TagImpl t = new TagImpl(node.getKey());
 		if (node instanceof BranchNode) {
-			((BranchNode) node).forEach(o -> fillTag(t, o));
+			((BranchNode) node).nodes().forEach(o -> fillTag(t, o));
 		}
 		return t;
 	}
 
 	private static void fillTag(Tag t, Node node) {
-		String k = node.getKey().toLowerCase();
+		String k = node.getKey();
 		if (node instanceof BranchNode) {
-			Tag n = t.createTag(k);
-			((BranchNode) node).forEach(o -> fillTag(n, o));
+			if (Attributes.containsAttr(k)) {
+				((BranchNode) node).nodes().stream().filter(o -> o instanceof LeafNode).map(o -> ((LeafNode) o)).forEach(o -> setAttribute(t, o.getKey(), o.getEntity()));
+			} else {
+				Tag n = t.createTag(k);
+				((BranchNode) node).nodes().forEach(o -> fillTag(n, o));
+			}
 		} else if (node instanceof LeafNode) {
 			Entity entity = ((LeafNode) node).getEntity();
-			if (Attributes.isAttr(k) || t.isAttribute(k)) {
-				if (entity != null) {
-					t.setAttribute(k, entity.getValue());
-				}
+			if (Attributes.isAttr(k) || ElementAttributes.isAttribute(t.getTagName(), k)) {
+				setAttribute(t, k, entity);
 			} else {
 				Tag n = t.createTag(k);
 				if (entity != null) {
 					n.setTagValue(entity.getValue());
 				}
 			}
+		}
+	}
+
+	private static void setAttribute(Tag t, String k, Entity entity) {
+		if (entity != null) {
+			t.setAttribute(k, entity.getValue());
 		}
 	}
 
