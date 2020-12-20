97007fe2b6c2a7e21a3b808de6abf2a50ea888a4
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-12-20 17:07:59 +0800

    Update Constants.java

--- src/main/java/com/github/dev/objectnotation/Constants.java
+++ src/main/java/com/github/dev/objectnotation/Constants.java
@@ -5,14 +5,96 @@ package com.github.dev.objectnotation;
  */
 public final class Constants {
 
+	private Constants() {
+	}
+
+	/**
+	 * The maximum length.
+	 */
+	public static final int MAX_LENGTH = 64;
+
 	/**
 	 * The maximum length of offset.
 	 */
 	public static final int OFFSET_MAX_LENGTH = 10;
 
 	/**
-	 * The maximum length of key.
+	 * '\r'
 	 */
-	public static final int KEY_MAX_LENGTH = 32;
+	public static final int CR = '\r';
+
+	/**
+	 * '\n'
+	 */
+	public static final int LF = '\n';
+
+	/**
+	 * '#'
+	 */
+	public static final int NUMBERSIGN = '#';
+
+	/**
+	 * '~'
+	 */
+	public static final int TILDE = '~';
+
+	/**
+	 * ':'
+	 */
+	public static final int COLON = ':';
+
+	/**
+	 * '['
+	 */
+	public static final int BRACKET = '[';
+
+	/**
+	 * '\'
+	 */
+	public static final int BACKSLASH = '\\';
+
+	/**
+	 * ' '
+	 */
+	public static final int SPACE = ' ';
+
+	/**
+	 * '0'
+	 */
+	public static final int _0 = '0';
+
+	/**
+	 * '9'
+	 */
+	public static final int _9 = '9';
+
+	/**
+	 * ','
+	 */
+	public static final int COMMA = ',';
+
+	public static boolean isCRLF(int i) {
+		return i == CR || i == LF;
+	}
+
+	public static boolean isMark(int i) {
+		return i == BRACKET || i == BACKSLASH;
+	}
+
+	public static boolean isDigit(int i) {
+		return i >= _0 && i <= _9;
+	}
+
+	public static boolean validChar(int i) {
+		return !invalidChar(i);
+	}
+
+	public static boolean invalidChar(int i) {
+		return i == NUMBERSIGN || i == TILDE || i == COLON || isMark(i) || isCntrl(i);
+	}
+
+	private static boolean isCntrl(int i) {
+		return (i >= 0 && i <= 31) || i == 127;
+	}
 
 }
