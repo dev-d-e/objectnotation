181a30b0c896d440f10315097e253c02d4b893c1
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2020-12-20 17:16:54 +0800

    Update StandardResolver.java

--- src/main/java/com/github/dev/objectnotation/StandardResolver.java
+++ src/main/java/com/github/dev/objectnotation/StandardResolver.java
@@ -1,27 +1,35 @@
 package com.github.dev.objectnotation;
 
-import com.github.dev.objectnotation.tree.Document;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.IntConsumer;
+
 import com.github.dev.objectnotation.tree.DocumentFactory;
 
 /**
- * Resolve key and value pair.
+ * Constructs a {@code Parser}, Resolve key and text pair.
  */
 class StandardResolver {
 
 	private final Parser parser;
-	private final ValueParser valueParser;
-
-	private final DocumentFactory documentFactory = new DocumentFactory();
 
 	private int curOffset = -1;
+
 	private String curKey;
+
 	private StringBuilder builder;
 
-	/**
-	 * Constructs a {@code Parser} and a {@code ValueParser}.
-	 */
-	StandardResolver() {
-		valueParser = new ValueParser(i -> {
+	private boolean quickStop = true;
+
+	private final List<int[]> invalidChars = new ArrayList<>();
+
+	StandardResolver(DocumentFactory documentFactory) {
+		parser = new Parser(o -> documentFactory.add(o), (i, s) -> {
+			curOffset = i;
+			curKey = s;
+			builder = new StringBuilder();
+		}, i -> {
 			if (builder != null) {
 				if (i == -1) {
 					documentFactory.addNode(curOffset, curKey, builder);
@@ -29,26 +37,59 @@ class StandardResolver {
 				} else if (i == -2) {
 					documentFactory.addNode(curOffset, curKey, builder);
 					builder = new StringBuilder();
-				} else {
-					builder.append((char) i);
 				}
 			}
-		});
-		parser = new Parser((i, s) -> {
+		}, i -> {
+			if (builder != null) {
+				builder.append((char) i);
+			}
+		}, (i, j) -> setErr(i, j));
+	}
+
+	StandardResolver(IntStringConsumer keyConsumer, IntConsumer textConsumer) {
+		parser = new Parser(o -> {
+		}, (i, s) -> {
 			curOffset = i;
 			curKey = s;
-			builder = new StringBuilder();
+			keyConsumer.accept(i, s);
 		}, i -> {
-			valueParser.apply(i);
-		});
+			if (i == -2) {
+				keyConsumer.accept(curOffset, curKey);
+			}
+		}, textConsumer, (i, j) -> setErr(i, j));
 	}
 
 	public void apply(int i) {
 		parser.apply(i);
 	}
 
-	public Document getDocument() {
-		return documentFactory.getDocument();
+	public StandardResolver setQuickStop(boolean quickStop) {
+		this.quickStop = quickStop;
+		return this;
+	}
+
+	private void setErr(int i, int j) {
+		if (invalidChars.size() < 100) {
+			invalidChars.add(new int[] { i, j });
+		}
+		if (quickStop) {
+			throw new RuntimeException(exceptionStr());
+		}
+	}
+
+	private String exceptionStr() {
+		StringBuilder str = new StringBuilder("invalid char is probably at");
+		invalidChars.forEach(e -> {
+			str.append('(').append(e[0]).append(',').append(e[1]).append(')');
+		});
+		return str.toString();
+	}
+
+	public Optional<String> getExceptionStr() {
+		if (invalidChars.isEmpty()) {
+			return Optional.empty();
+		}
+		return Optional.of(exceptionStr());
 	}
 
 }
