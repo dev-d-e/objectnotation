a9b36be0e89eb863c8cc7c50ec4d9067748fc6f5
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2023-12-16 22:28:27 +0800

    Update DocumentFactory.java

--- src/main/java/com/github/dev/objectnotation/tree/DocumentFactory.java
+++ src/main/java/com/github/dev/objectnotation/tree/DocumentFactory.java
@@ -1,8 +1,13 @@
 package com.github.dev.objectnotation.tree;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Objects;
 
+import com.github.dev.objectnotation.Contents;
 import com.github.dev.objectnotation.Header;
+import com.github.dev.objectnotation.Target;
 
 /**
  * Document factory.
@@ -11,91 +16,110 @@ public final class DocumentFactory {
 
 	private final DocumentImpl document = new DocumentImpl();
 
-	private boolean isBranch;
-	private BranchNode lastBranch;
-	private LeafNode lastLeaf;
+	private Node lastNode;
 
 	public Document getDocument() {
 		return document;
 	}
 
-	private void addBranch(int offset, String key) {
-		Objects.requireNonNull(key);
-		if (offset < 0) {
-			return;
-		}
-		BranchNode node = new BranchNodeImpl(offset, key);
-		if (offset == 0) {
-			document.add(node);
-			lastBranch = node;
-			isBranch = true;
-			return;
-		}
-		if (isBranch) {
-			if (offset == (lastBranch.getOffset() + 1)) {
-				lastBranch.add(node);
-			} else {
-				Node p = lastBranch.getNodeByOffset(offset);
-				if (p == null) {
-					return;
-				}
-				p.getParent().add(node);
-			}
-		} else {
-			Node p = lastLeaf.getNodeByOffset(offset);
-			if (p == null) {
-				return;
-			}
-			p.getParent().add(node);
-		}
-		lastBranch = node;
-		isBranch = true;
+	public List<Target> build() {
+		return build(document.getNodes());
 	}
 
-	private void addLeaf(int offset, String key, CharSequence cs) {
-		Objects.requireNonNull(key);
-		if (offset < 0) {
-			return;
-		}
-		LeafNode node = new LeafNodeImpl(offset, key, cs);
-		if (offset == 0) {
-			document.add(node);
-			lastLeaf = node;
-			isBranch = false;
-			return;
-		}
-		if (isBranch) {
-			if (offset == (lastBranch.getOffset() + 1)) {
-				lastBranch.add(node);
-			} else {
-				Node p = lastBranch.getNodeByOffset(offset);
-				if (p == null) {
-					return;
-				}
-				p.getParent().add(node);
-			}
-		} else {
-			Node p = lastLeaf.getNodeByOffset(offset);
-			if (p == null) {
-				return;
-			}
-			p.getParent().add(node);
-		}
-		lastLeaf = node;
-		isBranch = false;
+	List<Target> build(LinkedHashMap<String, List<Node>> nodes) {
+		List<Target> rst = new ArrayList<>();
+		nodes.forEach((k, v) -> {
+			Target o = new Target(k);
+			v.forEach(node -> {
+				node.getText().ifPresent(s -> o.getText().add(s));
+				build(node.getNodes()).forEach(e -> o.getValue().add(e));
+			});
+		});
+		return rst;
 	}
 
 	public void addNode(int offset, String key, CharSequence cs) {
-		if (cs == null || cs.length() == 0) {
-			addBranch(offset, key);
-		} else {
-			addLeaf(offset, key, cs);
+		Objects.requireNonNull(key);
+		if (offset < 0) {
+			return;
 		}
+		Node node = new AbstractNode(offset, key, cs);
+		if (offset == 0) {
+			document.add(node);
+			lastNode = node;
+			return;
+		}
+		if (offset == (lastNode.getOffset() + 1)) {
+			lastNode.add(node);
+		} else {
+			Node p = lastNode.getParentByOffset(offset);
+			if (p == null) {
+				return;
+			}
+			p.add(node);
+		}
+		lastNode = node;
 	}
 
 	public void add(Header header) {
 		header.getConfiguration().forEach(s -> document.configure(s, s));
 		header.getExternalResources().forEach(s -> document.externalResource(s));
+	}
+
+	public Contents getContents() {
+		return new DocNode(this);
+	}
+
+}
+
+class DocNode implements Contents {
+	private DocumentFactory documentFactory;
+	private int offset;
+	private StringBuilder key = new StringBuilder();
+	private StringBuilder text = new StringBuilder();
+
+	DocNode(DocumentFactory documentFactory) {
+		this.documentFactory = documentFactory;
+	}
+
+	@Override
+	public void preKey(int offset) {
+		this.offset = offset;
+	}
+
+	@Override
+	public void key(int i) {
+		key.append((char) i);
+	}
+
+	@Override
+	public void postKey() {
+	}
+
+	@Override
+	public void preText() {
+	}
+
+	@Override
+	public void text(int i) {
+		text.append((char) i);
+	}
+
+	@Override
+	public void textArray() {
+		documentFactory.addNode(offset, key.toString(), text);
+		text = new StringBuilder();
+	}
+
+	@Override
+	public void postText() {
+		documentFactory.addNode(offset, key.toString(), text);
+		key = new StringBuilder();
+		text = new StringBuilder();
+	}
+
+	@Override
+	public void error(int row, int n) {
 	}
 
 }
