25bf175327fde13b949aff5d22622ab0bde8c7c7
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2023-12-16 22:37:14 +0800

    Update HtmlConverter.java

--- src/main/java/com/github/dev/objectnotation/html/HtmlConverter.java
+++ src/main/java/com/github/dev/objectnotation/html/HtmlConverter.java
@@ -3,11 +3,8 @@ package com.github.dev.objectnotation.html;
 import java.util.Optional;
 
 import com.github.dev.objectnotation.ResolveTextInvoker;
-import com.github.dev.objectnotation.tree.BranchNode;
 import com.github.dev.objectnotation.tree.Document;
-import com.github.dev.objectnotation.tree.LeafNode;
 import com.github.dev.objectnotation.tree.Node;
-import com.github.dev.objectnotation.tree.NodeUtils;
 
 /**
  * Converter.
@@ -28,32 +25,20 @@ public class HtmlConverter {
 
 	private static Element convert(Node node) {
 		Element e = new Element(node.getKey());
-		NodeUtils.ifBranch(node, branch -> branch.getAll().forEach(o -> fill(e, o)));
+		node.getAll().forEach(o -> fill(e, o));
 		return e;
 	}
 
 	private static void fill(Element e, Node node) {
-		NodeUtils.ifBranch(node, branch -> fill(e, branch));
-		NodeUtils.ifLeaf(node, leaf -> fill(e, leaf));
-	}
-
-	private static void fill(Element e, BranchNode node) {
 		String k = node.getKey();
 		if (Attributes.containsAttr(k)) {
-			node.getLeaf().forEach(o -> o.getText().ifPresent(s -> e.addAttr(o.getKey(), s)));
-		} else {
-			Element c = createElement(k, e);
-			node.getAll().forEach(o -> fill(c, o));
-		}
-	}
-
-	private static void fill(Element e, LeafNode node) {
-		String k = node.getKey();
-		if (Attributes.isAttr(k) || ElementAttributes.isAttr(e.getName(), k)) {
+			node.getAll().forEach(o -> o.getText().ifPresent(s -> e.addAttr(o.getKey(), s)));
+		} else if (Attributes.isAttr(k) || ElementAttributes.isAttr(e.getName(), k)) {
 			node.getText().ifPresent(s -> e.addAttr(k, s));
 		} else {
 			Element c = createElement(k, e);
 			node.getText().ifPresent(s -> c.setText(s));
+			node.getAll().forEach(o -> fill(c, o));
 		}
 	}
 
