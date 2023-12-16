5955b6f3c2b8af0537a22bf02c1098107d95eecf
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2023-12-16 22:26:30 +0800

    Update AbstractNode.java

--- src/main/java/com/github/dev/objectnotation/tree/AbstractNode.java
+++ src/main/java/com/github/dev/objectnotation/tree/AbstractNode.java
@@ -1,15 +1,27 @@
 package com.github.dev.objectnotation.tree;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.github.dev.objectnotation.TextTypeAdapter;
+
 /**
  * Abstract class for {@code Node}.
  */
-abstract class AbstractNode implements Node {
+class AbstractNode implements Node {
 
-	private BranchNode parent;
+	private Node parent;
 
 	private int offset;
 
 	private String key;
+
+	private List<String> text = new ArrayList<>();
+
+	private LinkedHashMap<String, List<Node>> nodes = new LinkedHashMap<>();
 
 	/**
 	 * Constructor.
@@ -22,13 +34,18 @@ abstract class AbstractNode implements Node {
 		this.key = key;
 	}
 
+	AbstractNode(int offset, String key, CharSequence cs) {
+		this(offset, key);
+		setText(cs);
+	}
+
 	@Override
-	public BranchNode getParent() {
+	public Node getParent() {
 		return parent;
 	}
 
 	@Override
-	public Node setParent(BranchNode node) {
+	public Node setParent(Node node) {
 		this.parent = node;
 		return this;
 	}
@@ -39,12 +56,12 @@ abstract class AbstractNode implements Node {
 	}
 
 	@Override
-	public Node getNodeByOffset(int seekoffset) {
+	public Node getParentByOffset(int seekoffset) {
 		if (this.offset == seekoffset) {
-			return this;
+			return getParent();
 		}
 		if (seekoffset > 0 && seekoffset < this.offset) {
-			return getParent().getNodeByOffset(seekoffset);
+			return getParent().getParentByOffset(seekoffset);
 		}
 		return null;
 	}
@@ -52,6 +69,54 @@ abstract class AbstractNode implements Node {
 	@Override
 	public String getKey() {
 		return key;
+	}
+
+	@Override
+	public Node add(Node node) {
+		String k = node.getKey();
+		List<Node> n = nodes.getOrDefault(k, new ArrayList<>());
+		n.add(node);
+		nodes.put(k, n);
+		node.setParent(this);
+		return this;
+	}
+
+	@Override
+	public List<Node> getAll() {
+		List<Node> rst=new ArrayList<>();
+		nodes.values().forEach(e->rst.addAll(e));
+		return rst;
+	}
+
+	@Override
+	public LinkedHashMap<String, List<Node>> getNodes() {
+		return nodes;
+	}
+
+	@Override
+	public List<Node> get(String key) {
+		return nodes.get(key).stream().filter(o -> o.getKey().equals(key)).collect(Collectors.toList());
+	}
+
+	@Override
+	public Node setText(CharSequence cs) {
+		if (cs != null) {
+			this.text.add(cs.toString());
+		}
+		return this;
+	}
+
+	@Override
+	public Optional<String> getText() {
+		if (text.isEmpty()) {
+			return Optional.ofNullable(null);
+		}
+		return Optional.ofNullable(text.get(0));
+	}
+
+	@Override
+	public TextTypeAdapter getTypeAdapter() {
+		return TextTypeAdapter.of(getText().get());
 	}
 
 }
