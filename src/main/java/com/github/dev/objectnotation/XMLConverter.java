9ad096a929a4039bec94762101bd5b4e78356a18
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2023-12-16 22:12:52 +0800

    Update XMLConverter.java

--- src/main/java/com/github/dev/objectnotation/XMLConverter.java
+++ src/main/java/com/github/dev/objectnotation/XMLConverter.java
@@ -16,19 +16,69 @@ public final class XMLConverter {
 	 * @param charSequence the text.
 	 */
 	public static String convert(CharSequence charSequence) {
-		LinkedList<String> keys = new LinkedList<>();
-		StringBuilder builder = new StringBuilder();
-		ResolveTextInvoker.accept(charSequence, (i, s) -> {
-			while (i <= keys.size() - 1) {
-				builder.append('<');
-				builder.append(keys.pop());
-				builder.append("/>");
-			}
-			keys.push(s);
+		Converter c = new Converter();
+		ResolveTextInvoker.accept(charSequence, c);
+		return c.toXML();
+	}
+
+}
+
+class Converter implements Contents {
+	LinkedList<String> keys = new LinkedList<>();
+	int i;
+	StringBuilder key = new StringBuilder();
+	StringBuilder builder = new StringBuilder();
+
+	@Override
+	public void preKey(int offset) {
+		this.i = offset;
+	}
+
+	@Override
+	public void key(int i) {
+		key.append((char) i);
+	}
+
+	@Override
+	public void postKey() {
+		while (i <= keys.size() - 1) {
 			builder.append('<');
-			builder.append(s);
-			builder.append('>');
-		}, i -> builder.append((char) i));
+			builder.append(keys.pop());
+			builder.append("/>");
+		}
+		keys.push(key.toString());
+		builder.append('<');
+		builder.append(key);
+		builder.append('>');
+		key = new StringBuilder();
+	}
+
+	@Override
+	public void preText() {
+
+	}
+
+	@Override
+	public void text(int i) {
+		builder.append((char) i);
+	}
+
+	@Override
+	public void textArray() {
+
+	}
+
+	@Override
+	public void postText() {
+
+	}
+
+	@Override
+	public void error(int row, int n) {
+
+	}
+
+	public String toXML() {
 		while (keys.size() > 0) {
 			builder.append('<');
 			builder.append(keys.pop());
