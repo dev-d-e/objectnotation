09ebe36384bad9f4d23efe2c822eec0eec80ab75
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2023-12-21 16:50:24 +0800

    Update StandardResolver.java

--- src/main/java/com/github/dev/objectnotation/StandardResolver.java
+++ src/main/java/com/github/dev/objectnotation/StandardResolver.java
@@ -3,8 +3,7 @@ package com.github.dev.objectnotation;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-import com.github.dev.objectnotation.tree.DocumentFactory;
+import java.util.function.Consumer;
 
 /**
  * Constructs a {@code Parser}, Resolve key and text pair.
@@ -13,50 +12,17 @@ class StandardResolver {
 
 	private final Parser parser;
 
-	private boolean quickStop = true;
-
-	private final List<int[]> invalidChars = new ArrayList<>();
-
-	StandardResolver(DocumentFactory documentFactory) {
-		parser = new Parser(o -> documentFactory.add(o), documentFactory.getContents());
+	StandardResolver(Consumer<Header> consumer, Contents contents) {
+		parser = new Parser(consumer, contents);
 	}
 
 	StandardResolver(Contents contents) {
-		parser = new Parser(o -> {
+		this(o -> {
 		}, contents);
 	}
 
 	public void apply(int i) {
-		parser.apply(i);
-	}
-
-	public StandardResolver setQuickStop(boolean quickStop) {
-		this.quickStop = quickStop;
-		return this;
-	}
-
-	private void setErr(int i, int j) {
-		if (invalidChars.size() < 100) {
-			invalidChars.add(new int[] { i, j });
-		}
-		if (quickStop) {
-			throw new RuntimeException(exceptionStr());
-		}
-	}
-
-	private String exceptionStr() {
-		StringBuilder str = new StringBuilder("invalid char is probably at");
-		invalidChars.forEach(e -> {
-			str.append('(').append(e[0]).append(',').append(e[1]).append(')');
-		});
-		return str.toString();
-	}
-
-	public Optional<String> getExceptionStr() {
-		if (invalidChars.isEmpty()) {
-			return Optional.empty();
-		}
-		return Optional.of(exceptionStr());
+		parser.apply((char) i);
 	}
 
 }
