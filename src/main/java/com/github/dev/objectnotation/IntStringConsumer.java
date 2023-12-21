57d44a98ed0b524e6f0a21f8149ad4b3a57ad200
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2023-12-21 16:57:04 +0800

    Update IntStringConsumer.java

--- src/main/java/com/github/dev/objectnotation/IntStringConsumer.java
+++ src/main/java/com/github/dev/objectnotation/IntStringConsumer.java
@@ -1,18 +1,1 @@
-package com.github.dev.objectnotation;
 
-/**
- * Represents an operation that accepts a int-valued argument and a
- * string-valued argument, and returns no result.
- */
-@FunctionalInterface
-public interface IntStringConsumer {
-
-	/**
-	 * Performs this operation on the given arguments.
-	 *
-	 * @param i the first input argument
-	 * @param s the second input argument
-	 */
-	void accept(int i, String s);
-
-}
