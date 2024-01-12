3c07e5d440e4c2972e7209cd44270e2420589f72
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2024-01-12 18:55:11 +0800

    Update Constants.java

--- src/main/java/com/github/dev/objectnotation/Constants.java
+++ src/main/java/com/github/dev/objectnotation/Constants.java
@@ -11,54 +11,59 @@ public final class Constants {
 	/**
 	 * '\r'
 	 */
-	public static final int CR = '\r';
+	public static final char CR = '\r';
 
 	/**
 	 * '\n'
 	 */
-	public static final int LF = '\n';
+	public static final char LF = '\n';
 
 	/**
 	 * '#'
 	 */
-	public static final int NUMBERSIGN = '#';
+	public static final char NUMBERSIGN = '#';
 
 	/**
 	 * '~'
 	 */
-	public static final int TILDE = '~';
+	public static final char TILDE = '~';
 
 	/**
 	 * ':'
 	 */
-	public static final int COLON = ':';
+	public static final char COLON = ':';
 
 	/**
 	 * '|'
 	 */
-	public static final int VERTICAL = '|';
+	public static final char VERTICAL = '|';
 
 	/**
 	 * '+'
 	 */
-	public static final int PLUS = '+';
+	public static final char PLUS = '+';
 
 	/**
 	 * ' '
 	 */
-	public static final int SPACE = ' ';
+	public static final char SPACE = ' ';
 
 	/**
 	 * '\'
 	 */
-	public static final int BACKSLASH = '\\';
+	public static final char BACKSLASH = '\\';
 
-	public static boolean isCRLF(int i) {
-		return i == CR || i == LF;
+	/**
+	 * 0
+	 */
+	public static final char END = 0;
+
+	public static boolean isCRLF(char c) {
+		return c == CR || c == LF;
 	}
 
-	public static boolean isDigit(int i) {
-		return (i >= '0' && i <= '9') || (i >= 'A' && i <= 'F') || (i >= 'a' && i <= 'f');
+	public static boolean isDigit(char c) {
+		return (c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
 	}
 
 }
