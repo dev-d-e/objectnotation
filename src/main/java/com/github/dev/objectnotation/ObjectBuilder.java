20f0f78b837b01b0057813a26d46544a46cd7ab8
dev-d-e <66547986+dev-d-e@users.noreply.github.com>
2024-01-12 18:58:28 +0800

    Update ObjectBuilder.java

--- src/main/java/com/github/dev/objectnotation/ObjectBuilder.java
+++ src/main/java/com/github/dev/objectnotation/ObjectBuilder.java
@@ -5,79 +5,100 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-import com.github.dev.objectnotation.ClassProperty.Property;
-import com.github.dev.objectnotation.tree.BranchNode;
-import com.github.dev.objectnotation.tree.Document;
-import com.github.dev.objectnotation.tree.LeafNode;
-import com.github.dev.objectnotation.tree.Node;
-import com.github.dev.objectnotation.tree.NodeUtils;
-
+/**
+ * Set Bean.
+ */
 final class ObjectBuilder {
 
-	static <T> T build(Document document, T object) {
+	/**
+	 * Build T object by Target.
+	 *
+	 * @param target Target
+	 * @param object T
+	 * @return T
+	 */
+	static <T> T build(Target target, T object) {
 		Objects.requireNonNull(object);
-		return buildForObject(document.nodes(), object);
+		if (object.getClass().getSimpleName().equalsIgnoreCase(target.getName())) {
+			return buildListForObject(target.getValue(), object);
+		}
+		return buildForObject(target, object);
 	}
 
-	static <T> T build(Document document, Class<T> type) {
+	/**
+	 * Build T object by Target.
+	 *
+	 * @param target Target
+	 * @param type   Class
+	 * @return T
+	 */
+	static <T> T build(Target target, Class<T> type) {
 		Objects.requireNonNull(type);
-		return buildByClass(document.nodes(), type);
+		return buildByClass(target, type);
 	}
 
-	static <T> T build(List<Node> nodes, T object) {
-		Objects.requireNonNull(object);
-		return buildForObject(nodes, object);
-	}
-
-	static <T> T build(List<Node> nodes, Class<T> type) {
-		Objects.requireNonNull(type);
-		return buildByClass(nodes, type);
-	}
-
-	private static <T> T buildByClass(List<Node> nodes, Class<T> type) {
+	/**
+	 * Build T object by Target.
+	 */
+	private static <T> T buildByClass(Target target, Class<T> type) {
 		try {
 			T o = type.getDeclaredConstructor().newInstance();
-			return buildForObject(nodes, o);
+			if (type.getSimpleName().equalsIgnoreCase(target.getName())) {
+				return buildListForObject(target.getValue(), o);
+			}
+			return buildForObject(target, o);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
-	private static <T> T buildForObject(List<Node> nodes, T object) {
-		ClassProperty p = new ClassProperty(object.getClass());
-		nodes.forEach(node -> {
-			p.getProperty(node.getKey()).ifPresent(property -> {
-				NodeUtils.ifBranch(node, o -> buildForNode(object, property, o));
-				NodeUtils.ifLeaf(node, o -> buildForNode(object, property, o));
-			});
+	/**
+	 * each target is a field in T object.
+	 */
+	private static <T> T buildListForObject(List<Target> targetList, T object) {
+		targetList.forEach(t -> {
+			buildForObject(t, object);
 		});
 		return object;
 	}
 
-	private static void buildForNode(Object object, Property property, BranchNode node) {
-		Class<?> c = property.getType();
-		List<Node> childNodes = node.getAll();
-		if (c.isArray()) {
-			property.setForArray(object, buildByClass(childNodes, c.getComponentType()));
-		} else if (Collection.class.isAssignableFrom(c)) {
-			property.setForCollection(object, t -> buildByClass(childNodes, t));
-		} else if (Map.class.isAssignableFrom(c)) {
-			property.setForMap(object, t -> buildByClass(childNodes, t));
-		} else {
-			property.setForBean(object, t -> buildByClass(childNodes, t), o -> buildForObject(childNodes, o));
-		}
-	}
-
-	private static void buildForNode(Object object, Property property, LeafNode node) {
-		Class<?> c = property.getType();
-		if (c.isArray()) {
-			property.setForArrayByType(object, node.getTypeAdapter());
-		} else if (Collection.class.isAssignableFrom(c)) {
-			property.setForCollectionByType(object, node.getTypeAdapter());
-		} else {
-			property.invokeWriteMethodByType(object, node.getTypeAdapter());
-		}
+	/**
+	 * a target is a field in T object.
+	 */
+	private static <T> T buildForObject(Target target, T object) {
+		ClassProperty p = new ClassProperty(object.getClass());
+		p.getProperty(target.getName()).ifPresent(property -> {
+			Class<?> c = property.getType();
+			List<String> text = target.getText();
+			if (text.size() > 0) {
+				if (c.isArray()) {
+					text.forEach(s -> {
+						property.setForArrayByType(object, TextTypeAdapter.of(s));
+					});
+				} else if (Collection.class.isAssignableFrom(c)) {
+					text.forEach(s -> {
+						property.setForCollectionByType(object, TextTypeAdapter.of(s));
+					});
+				} else {
+					text.forEach(s -> {
+						property.invokeWriteMethodByType(object, TextTypeAdapter.of(s));
+					});
+				}
+			} else {
+				if (c.isArray()) {
+					property.setForArray(object, buildByClass(target, c.getComponentType()));
+				} else if (Collection.class.isAssignableFrom(c)) {
+					property.setForCollection(object, t -> buildByClass(target, t));
+				} else if (Map.class.isAssignableFrom(c)) {
+					property.setForMap(object, t -> buildByClass(target, t));
+				} else {
+					property.setForBean(object, t -> buildByClass(target, t),
+							o -> buildListForObject(target.getValue(), o));
+				}
+			}
+		});
+		return object;
 	}
 
 }
